0eb6b56cf3382af11d2f9a6e118eed84a6eb3953
yasin <yashoss@gmail.com>
2017-06-21 12:17:19 -0700

    Connected peers

--- client.js
+++ client.js
@@ -85,3 +85,47 @@ function send(message) {
 
   connection.send(JSON.stringify(message));
 };
+
+connectToOtherUsernameBtn.addEventListener("click", function(){
+  var otherUsername = otherUsernameInput.value;
+  connectedUser = otherUsername;
+
+  if(otherUsername.length > 0) {
+    myConnection.createOffer(function(offer) {
+      console.log();
+      send({
+        type: "offer",
+        offer: offer
+      });
+
+      myConnection.setLocalDescription(offer);
+    }, function(error){
+      alert("An error has occured.");
+    });
+  }
+})
+
+function onOffer(offer, name) {
+  connectedUser = name;
+  myConnection.setRemoteDescription(new RTCSessionDescription(offer));
+
+  myConnection.createAnswer(function(answer) {
+    myConnection.setLocalDescription(answer);
+
+    send({
+      type: "answer",
+      answer: answer
+    });
+
+  }, function(error) {
+    alert("oops...error");
+  });
+}
+
+function onAnswer(answer) {
+  myConnection.setRemoteDescription(new RTCSessionDescription(answer));
+}
+
+function onCandidate(candidate) {
+  myConnection.addIceCandidate(new RTCIceCandidate(candidate));
+}
